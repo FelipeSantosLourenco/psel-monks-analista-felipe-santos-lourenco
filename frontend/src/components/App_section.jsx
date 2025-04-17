a35e45b8f0d600054fc488385a51b1593d1dc1c0
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-04-16 22:05:30 -0300

    add app section component

---
+++ frontend/src/components/App_section.jsx
@@ -0,0 +1,15 @@
+export default function AppSection() {
+    return (
+        <section className="bg-[#eae8e4] py-12">
+            <div className= "bg-[#3C0C60] ">
+                <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aspernatur dolore magni porro nostrum qui nobis adipisci.</p>
+            </div>
+
+            <div>
+                <button ></button>
+                <button></button>
+            </div>
+        </section>
+    )
+}
